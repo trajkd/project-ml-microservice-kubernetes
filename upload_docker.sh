9bd4d665c67d0967e5bc02020c9b90f4622c7295
trajkd <trajkd@gmail.com>
2020-06-28 20:01:01 +0200

    Initial commit

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="ciaodejan/ml-app"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag ml-app:latest $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
